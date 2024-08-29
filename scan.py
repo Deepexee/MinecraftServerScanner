4e2a1f4a81236d8136ed6aa7db76bdb3f0c8721f
Deepexe <79488121+Deepexee@users.noreply.github.com>
2024-08-29 14:29:47 +0200

    Update scan.py
    Vérification de la version

--- scan.py
+++ scan.py
@@ -1,45 +1,41 @@
-# J'ai décris toutes les parties du scripts pour que tout le monde puisse comprendre
-# n'hésitez pas a proposer des mise a jour pour crée quelque chose de vraiment optimiser
-
-# https://twitch.tv/deepexee
-
-import random # l'aléatoire
-import socket # La connexion
-import threading, os # La puissance et les commandes de base de windows
-from colorama import Fore, init # La couleur
+import random
+import socket
+import threading, os
+from colorama import Fore, init
+import minestat
 
 init()
 
-ports = [25565, 25566, 25567] # Vous pouvez changer les port pour scanner d'autres choses, 80 & 443 = http, 2024 ou 22 = ssh
-output_file = "hit.txt" # Nom du fichier
-num_threads = 2500 # Changez cette valeur pour augementer la puissance
+ports = [25565, 25566, 25567]
+output_file = "hit.txt"
+num_threads = 500
 
 def generate_ip():
-    return '.'.join(str(random.randint(0, 255)) for _ in range(4)) # Crée une adresse ip aléatoire
+    return '.'.join(str(random.randint(0, 255)) for _ in range(4))
 
 def scan_ip(ports):
-    while True: # Crée une boucle
-        ip = generate_ip() # La variable Ip appelle la fonction Ligne 17 qui crée une adresse ip 
+    while True:
+        ip = generate_ip()
         for port in ports:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.settimeout(1)
-            result = sock.connect_ex((ip, port)) # Ce connecte
-            if result == 0: # Si le script arrive a ce connecter
-                print(f"{ip} is a Minecraft server!") # Affiche dans la console
-                with open(output_file, 'a') as file: # Ouvre le fichier texte
-                    file.write(f"{ip}:{port}\n") # Ecris dans le fichier texte 
-                sock.close()
+            result = sock.connect_ex((ip, port))
+            if result == 0:
+                ms = minestat.MineStat(ip, port)
+                with open(output_file, 'a') as file:
+                    if ms.version != "None":
+                        print(f"{ip} is a Minecraft server!")
+                        file.write(f"{ip}:{port}\nVersion: {ms.version}\n")
+                    else:
+                        sock.close()
+                        pass
                 return
-            else: # Sinon
-                pass # Continue a chercher
             sock.close()
 
 def thread_worker():
     scan_ip(ports)
 
 if __name__ == "__main__":
-    os.system('title MinecraftScan - twitch.tv/deepexee') # je change le nom de la fenetre
-    os.system('start https://twitch.tv/deepexee') # Ouvre mon twitch (faut que je mange a la fin du mois)
 
     threads = []
     for _ in range(num_threads):
